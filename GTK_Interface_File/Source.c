80b24271c410539860b06716f19c33bc224d177a
Louis Plaire <louisplaire14@gmail.com>
2025-10-30 15:04:16 +0100

    Update Source.c
    wrong include for Header.h

--- GTK_Interface_File/Source.c
+++ GTK_Interface_File/Source.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "header.h"
+#include "Header.h"
 
 /*
 void Clean_Ressources(SDL_Window* w, SDL_Renderer* r, SDL_Texture* t) {  // Instead of Destroying manually, this function do it for us 
